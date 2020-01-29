72b8cf60e6b88ea89095670bca1d4aa3a180a12d
Daniel Smith <DANSMITH@meditech.com>
2020-01-29 12:17:01 -0500

    initial changes for beyond compare extension

--- README.md
+++ README.md
@@ -7,46 +7,54 @@ There are five commands to do different ways to choose the files for comparing. 
 Hit `Ctrl` + `Shift` + `P` to open the command menu and type `Meld Diff`.
 
 ![All commands](images/all_cmds.png)
-
 *Folder comparison is only possible from file list (see hint in command description 'Select for meld compare' and 'Compare with selected for meld compare')*
 
-# Detailed command information
-## All visible documents
+## Detailed command information
+
+### All visible documents
+
 To use this command two files have to be visible side by side.
 
 ![Compare side by side files](images/files_side_by_side.png)
 
-## Current file to one other open file in editor
+### Current file to one other open file in editor
+
 The current selected file will be the first to compare (left side in meld).
 The second file to compare (right side in meld) is selected by menu which shows all open files.
 
 ![Compare file to other open file](images/compare_to_open_file.png)
 
-## Current file to one other file (not open in editor)
+### Current file to one other file (not open in editor)
+
 The current selected file will be the first to compare (left side in meld).
 The second file to compare (right side in meld) is selected by open file dialog.
 
-## Select for meld compare
+### Select for meld compare
+
 Set the current selected file as the first to compare (left side in meld).
 
 This is also possible in the file list.
 
 ![Compare file to compare to from file list](images/select_for_compare.png)
 
-## Compare with selected for meld compare
+### Compare with selected for meld compare
+
 Compare current file (right side in meld) to the file selected before.
 
 This is also possible in the file list.
 
 ![Compare to selected from file list](images/compare_to_selected.png)
 
-# Usage with file list (also folder comparison possible)
-## Select for meld compare
+## Usage with file list (also folder comparison possible)
+
+### Select for meld compare
+
 Select the the first file or folder to compare (left side in meld) by right click on file list and hit '*Select for meld compare*'.
 
 ![Compare file to compare to from file list](images/select_for_compare.png)
 
-## Compare with selected for meld compare
+### Compare with selected for meld compare
+
 Select the file or folder to compare to the file or folder selected before (right side in meld) by right click on file list and hit '*Compare with selected for meld compare*'.
 
 ![Compare to selected from file list](images/compare_to_selected.png)
--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,27 @@ import { window, commands } from 'vscode';
 import { posix } from 'path';
 import { existsSync, open } from 'fs';
 
-const cp = require('child_process')
-let fillListDone = false
+const cp = require('child_process');
+let fillListDone = false;
 
-export function showMeld(file1: String, file2: String) {
-	cp.exec('meld ' + file1+ " " + file2, (error: Error) => {
+//const pathHint = configuration.get<string>("path");
+const svnConfig = vscode.workspace.getConfiguration("svn");
+const bcConfig = vscode.workspace.getConfiguration("beyond-compare-diff");
+const defaultConfig = vscode.workspace.getConfiguration("");
+
+const nil = "";
+
+export function showBeyondCompare(files: String[]) {
+	//"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly
+	const beyondCompare = '"C:\\Program Files (x86)\\Beyond Compare 4\\BComp.exe"';
+	const command = beyondCompare + ' ' + files.join(' ');
+	console.log(command);
+	cp.exec(command, (error: Error)=> {
 		if (error) {
-			if (error.message.match(/meld: not found/)) {
-				window.showErrorMessage("Meld Diff: meld is not installed!");
-			}
+			 if (error.message.match(/is not recognized/)) {
+			 	window.showErrorMessage("Beyond Compare is not installed! (" +beyondCompare +")");			
+			 }
+//			window.showErrorMessage(error.message);
 		}
 	});
 }
@@ -20,7 +32,7 @@ export function showMeld(file1: String, file2: String) {
 export function showListAndDiff(current: String, possible_diffs: String[]) {
 	// remove current editor
 	let possible = possible_diffs.filter(function(value, index, arr) {
-		return value != current;
+		return value !== current;
 	});
 
 	let a = Array();
@@ -32,7 +44,7 @@ export function showListAndDiff(current: String, possible_diffs: String[]) {
 		placeHolder: 'Filename to diff'
 	}).then(result => {
 		if (existsSync(result)) {
-			showMeld(current, result);
+			showBeyondCompare([current, result]);
 		}
 	});
 }
@@ -47,7 +59,7 @@ export function doIt(current: String, possible_diffs: String[]) {
 		}
 		commands.executeCommand("workbench.action.nextEditor").then(_ => {
 			if (window.activeTextEditor) {
-				if (window.activeTextEditor.document.fileName != current) {
+				if (window.activeTextEditor.document.fileName !== current) {
 					doIt(current, possible_diffs);
 				} else {
 					fillListDone = true;
@@ -75,41 +87,35 @@ export function activate(context: vscode.ExtensionContext) {
 
 	vscode.workspace.onDidCloseTextDocument(event => {
 		//remove file from list on closing
-		let index = open_files_event.indexOf(event.fileName)
+		let index = open_files_event.indexOf(event.fileName);
 		if (fillListDone && index !== -1) {
 			open_files_event.splice(index, 1);
 		}
 	});
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffVisible', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('beyond-compare-diff.diffVisible', () => {
 		let open_files: String[] = [];
 		window.visibleTextEditors.forEach(editor => {
 			open_files.push(editor.document.fileName.toString());
 		});
 
 		if (open_files.length < 2) {
-			window.showErrorMessage("Meld Diff:\nCan't diff: Only one file is visible in editor!")
+			window.showErrorMessage("Beyond Compare:\nCan't diff: Only one file is visible in editor!");
 			return;
 		}
 		if (open_files.length > 3) {
-			window.showErrorMessage("Meld Diff:\nCan't diff: More than three files are visible in editor!")
+			window.showErrorMessage("Beyond Compare:\nCan't diff: More than three files are visible in editor!");
 			return;
 		}
 
-		cp.exec('meld ' + open_files.join(" "), (error: Error) => {
-			if (error) {
-				if (error.message.match(/meld: not found/)) {
-					window.showErrorMessage("Meld Diff: meld is not installed!");
-				}
-			}
-		});
+		showBeyondCompare(open_files);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffCurrentToOtherOpen', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('beyond-compare-diff.diffCurrentToOtherOpen', () => {
 		let open_files: String[] = [];
 
 		if (!window.activeTextEditor) {
-			window.showErrorMessage("Meld Diff:\nCurrent window is not a file!")
+			window.showErrorMessage("Beyond Compare:\nCurrent window is not a file!");
 			return;
 		}
 
@@ -122,9 +128,9 @@ export function activate(context: vscode.ExtensionContext) {
 		doIt(current, open_files_event);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffCurrentToOther', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('beyond-compare-diff.diffCurrentToOther', () => {
 		if (!window.activeTextEditor) {
-			window.showErrorMessage("Meld Diff:\nNo file selected for diff!")
+			window.showErrorMessage("Beyond Compare:\nNo file selected for diff!");
 			return;
 		}
 
@@ -136,14 +142,15 @@ export function activate(context: vscode.ExtensionContext) {
 	   	};
 
 		window.showOpenDialog(options).then(_ => {
-			cp.exec('meld ' + current + " " + _);
+			showBeyondCompare([current, String(_)]);
 		});
 
 	}));
 
 	let selected:String = "";
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffFromFileListSelect', (_) => {
+	context.subscriptions.push(vscode.commands.registerCommand('beyond-compare-diff.diffFromFileListSelect', (_) => {
+		console.log('beyond-compare-diff.diffFromFileListSelect');
 		if (! _) {
 			if (window.activeTextEditor) {
 				selected = window.activeTextEditor.document.fileName;
@@ -151,11 +158,14 @@ export function activate(context: vscode.ExtensionContext) {
 				return;
 			}
 		} else {
-			selected = _.path;
+			selected = _.fsPath;
 		}
+
+		console.log('Selected: ' +selected);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('meld-diff.diffFromFileList', (_) => {
+	context.subscriptions.push(vscode.commands.registerCommand('beyond-compare-diff.diffFromFileList', (_) => {
+		console.log('beyond-compare-diff.diffFromFileList');
 		let path = "";
 		if (! _) {
 			if (window.activeTextEditor) {
@@ -164,12 +174,12 @@ export function activate(context: vscode.ExtensionContext) {
 				return;
 			}
 		} else {
-			path = _.path;
+			path = _.fsPath;
 		}
 		if (selected.length > 0) {
-			showMeld(selected, path);
+			showBeyondCompare([selected, path]);
 		} else {
-			window.showErrorMessage("Meld Diff: First select a file to compare to!")
+			window.showErrorMessage("Beyond Compare: First select a file to compare to!");
 		}
 	}));
 }
